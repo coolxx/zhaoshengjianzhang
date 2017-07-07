d2a5d572288cd901536b5185be603317c994403a
henghenghahadada <xing19931122@sina.com>
2017-07-07 12:04:24 +0800

    sadsadasdasda

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 $(function(){
+	var hg1=$('.main_cc').position().top;
+	var hg=$(window).height();
+	var fa_height=hg-hg1;
+	$('.posi_fa').css('height',fa_height);
 	$('.l_area p').each(function(index){
 		$(this).click(function(){
 			$('.l_area p').removeClass('on1').eq(index).addClass('on1');
